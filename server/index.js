f2ab1d57ee93830cfba3c28f204f07f56a91ca43
s2316889 <j.zheng-35@sms.ed.ac.uk>
2023-07-02 21:05:57 +0100

    user route halfway

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 import {register} from "./controller/auth.js"
 
 // configurations
@@ -37,6 +39,9 @@ const upload = multer({storage})
 // Routes with files
 app.post("auth/register", upload.single("picture"), register)
 
+// Routes
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
 
 // DB setup
 const PORT = process.env.PORT || 6001;
