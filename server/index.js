ae4f6174e70d29806c342e55a1f2e24bb2d97194
s2316889 <j.zheng-35@sms.ed.ac.uk>
2023-07-05 14:57:58 +0100

    debug complete

--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
-import {register} from "./controller/auth.js"
+import {register} from "./controllers/auth.js"
 import {createPost} from "./controllers/posts.js"
 import {verifyToken} from "./middleware/auth.js"
+import User from "./models/User.js"
+import Post from "./models/Post.js"
+import {users, posts} from "./data/index.js"
 
 // configurations
 const __filename = fileURLToPath(import.meta.url)
@@ -55,4 +58,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+    User.insertMany(users)
+    Post.insertMany(posts)
 }).catch(((error) => console.log(`${error} did not connect`)))
